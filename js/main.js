dd05405417a46539a947512ef67aadd62c31bdf7
Arif Ramadhani <62203067+arifrmdhni@users.noreply.github.com>
2020-04-22 20:02:53 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
+  // sticky navigation menu
+  let nav_offset_top = $('.header_area').height() + 50;
+
+  function navbarFixed() {
+    if ($('.header_area').length) {
+      $(window).scroll(function() {
+        let scroll = $(window).scrollTop();
+        if (scroll >= nav_offset_top) {
+          $('.header_area .main-menu').addClass('navbar_fixed');
+        } else {
+          $('.header_area .main-menu').removeClass('navbar_fixed');
+        }
+      });
+    }
+  }
+
+  navbarFixed();
+
+
 $(document).ready(function() {
   let $btns = $('.project-area .button-group button');
 
@@ -35,23 +54,7 @@ $(document).ready(function() {
     }
   });
 
-  // sticky navigation menu
-  let nav_offset_top = $('.header_area').height() + 50;
 
-  function navbarFixed() {
-    if ($('.header_area').length) {
-      $(window).scroll(function() {
-        let scroll = $(window).scrollTop();
-        if (scroll >= nav_offset_top) {
-          $('.header_area .main-menu').addClass('navbar_fixed');
-        } else {
-          $('.header_area .main-menu').removeClass('navbar_fixed');
-        }
-      });
-    }
-  }
-
-  navbarFixed();
 
   // Go to Top
   $(window).on('scroll', function() {
